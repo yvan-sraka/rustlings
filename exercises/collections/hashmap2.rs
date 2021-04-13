9453f86a621d53c63931cf84e8e5453a7dc4f8c1
maxime.dharboulle <maxime.dharboulle@sequodia.com>
2021-04-13 19:42:30 +0200

    init to quiz 2

--- exercises/collections/hashmap2.rs
+++ exercises/collections/hashmap2.rs
@@ -12,7 +12,6 @@
 // Execute the command `rustlings hint collections4` if you need
 // hints.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -35,6 +34,10 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
     ];
 
     for fruit in fruit_kinds {
+        if !basket.contains_key(&fruit) {
+            basket.insert(fruit, 1);
+        }
+
         // TODO: Put new fruits if not already present. Note that you
         // are not allowed to put any type of fruit that's already
         // present!
