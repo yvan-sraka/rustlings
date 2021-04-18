43f8b28db05272d3a2685b9fe73e7d70371162d5
Benoît Valle <benoit.valleea@hotmail.fr>
2021-04-18 17:33:49 +0200

    collections 2 done

--- exercises/collections/vec2.rs
+++ exercises/collections/vec2.rs
@@ -7,8 +7,6 @@
 // Execute the command `rustlings hint collections2` if you need
 // hints.
 
-// I AM NOT DONE
-
 fn vec_loop(mut v: Vec<i32>) -> Vec<i32> {
     for i in v.iter_mut() {
         // TODO: Fill this up so that each element in the Vec `v` is
