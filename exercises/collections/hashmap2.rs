8ae911ae7776c48482df158bf5f15bb1d205bc97
Benoît Valle <benoit.valleea@hotmail.fr>
2021-04-18 17:49:18 +0200

    collections 4 done

--- exercises/collections/hashmap2.rs
+++ exercises/collections/hashmap2.rs
@@ -38,6 +38,14 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         // TODO: Put new fruits if not already present. Note that you
         // are not allowed to put any type of fruit that's already
         // present!
+
+        match fruit {
+            Fruit::Apple => basket.insert(Fruit::Apple, 4),
+            Fruit::Banana => basket.insert(Fruit::Banana, 1),
+            Fruit::Mango => basket.insert(Fruit::Mango, 2),
+            Fruit::Lichi => basket.insert(Fruit::Lichi, 5),
+            Fruit::Pineapple => basket.insert(Fruit::Pineapple,1),
+        };
     }
 }
 
