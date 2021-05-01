98a2a8302ac021816b1c899bf155e8fc293303b2
Aristide Bamenou <aristide.bamenou@hotmail.com>
2021-05-02 01:04:19 +0200

    function

--- exercises/functions/functions1.rs
+++ exercises/functions/functions1.rs
@@ -1,7 +1,8 @@
 // functions1.rs
-// Make me compile! Execute `rustlings hint functions1` for hints :)
 
-// I AM NOT DONE
+fn call_me() {
+    println! ("I'm Aristide Bamenou")
+}
 
 fn main() {
     call_me();
