256c6f370f1bb1b04f699639f758223fff58deea
Remaetanju <remibraizet@gmail.com>
2021-05-08 22:21:29 +0200

    all bonuses done up to iterators1.rs included

--- exercises/standard_library_types/iterators2.rs
+++ exercises/standard_library_types/iterators2.rs
@@ -12,8 +12,9 @@
 pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
     match c.next() {
+
         None => String::new(),
-        Some(first) => first.collect::<String>() + c.as_str(),
+        Some(first) => first.to_uppercase().collect::<String>() + c.as_str(),
     }
 }
 
