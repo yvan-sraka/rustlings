174d4ce2b08a014c0baa409fc08d7d54a055789e
Herve HUANG <herve.huang@yahoo.fr>
2021-05-11 18:03:38 +0200

    Clean cache

--- exercises/collections/hashmap1.rs
+++ exercises/collections/hashmap1.rs
@@ -11,17 +11,21 @@
 // Execute the command `rustlings hint collections3` if you need
 // hints.
 
-// I AM NOT DONE
+
 
 use std::collections::HashMap;
 
 fn fruit_basket() -> HashMap<String, u32> {
-    let mut basket = // TODO: declare your hash map here.
+    let mut basket = HashMap::<String,u32>::new();// TODO: declare your hash map here.
 
     // Two bananas are already given for you :)
     basket.insert(String::from("banana"), 2);
 
     // TODO: Put more fruits in your basket here.
+    basket.insert(String::from("mango"), 1);
+    basket.insert(String::from("pear"), 1);
+    basket.insert(String::from("lemon"), 1);
+    basket.insert(String::from("cherry"), 2);
 
     basket
 }
--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -7,7 +7,6 @@
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the order amount. No hints this time!
 
-// I AM NOT DONE
 
 // Put your function here!
 // fn ..... {
@@ -20,4 +19,12 @@ fn verify_test() {
 
     assert_eq!(70, price1);
     assert_eq!(65, price2);
+}
+fn calculate_apple_price(coin : i32) -> i32
+{
+    if coin>40 {
+        coin
+    }else{
+        coin*2
+    }
 }
